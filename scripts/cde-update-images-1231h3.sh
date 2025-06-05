c74ad7368d7f3aba0bb011b66e98842570f03da1
Shanmugaprabhu <sramadoss@cloudera.com>
2025-06-05 15:29:15 -0500

    Update cde-update-images-1231h3.sh

--- scripts/cde-update-images-1231h3.sh
+++ scripts/cde-update-images-1231h3.sh
@@ -1,24 +1,65 @@
+#!/usr/bin/env bash
 # The steps will work irrespective of spark versions, since we are replacing only the image tag. It is not required to run these for Spark 2.x VCs
 
 # The following script works in bash, confirm if you are in bash shell and on latest bash version and given kubectl version. (It could work on other bash versions, but not tested)
 
-echo $0
-# bash
+echo "-------------------------------------"
+
+echo "Bash version"
 echo $BASH_VERSION
 # 5.2.37(1)-release
+echo "kubectl version"
 kubectl version --client
 # Client Version: v1.30.*
  
+echo "-------------------------------------"
 
 
 
-# Manual Inputs
-# TODO: Set the kubeconfig
-export KUBECONFIG=<path to your kubeconfig>
-# TODO: Set the Virtual cluster app id here.
-DEX_APP_ID=dex-app-<app_id>
 
+# Accept DEX_APP_ID as a parameter with validation
+if [ $# -eq 0 ]; then
+    echo "Error: Missing DEX_APP_ID parameter"
+    echo "Usage: $0 <dex-app-id>"
+    exit 1
+fi
+
+DEX_APP_ID=$1
+
+# Validate format
+if [[ ! $DEX_APP_ID =~ ^dex-app-[a-zA-Z0-9_-]+$ ]]; then
+    echo "Error: Invalid DEX_APP_ID format. It should be in the form 'dex-app-<app_id>'"
+    exit 1
+fi
+
+# Check for DEX_APP_ID and optional KUBECONFIG parameters
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "Error: Incorrect number of parameters"
+    echo "Usage: $0 <dex-app-id> [kubeconfig-path]"
+    exit 1
+fi
+
+DEX_APP_ID=$1
+
+# Set KUBECONFIG if provided as second parameter
+if [ $# -eq 2 ]; then
+    export KUBECONFIG=$2
+    if [ ! -f "$KUBECONFIG" ]; then
+        echo "Error: KUBECONFIG file not found: $KUBECONFIG"
+        exit 1
+    fi
+else
+    echo "Error: No KUBECONFIG provided"
+    echo "Usage: $0 <dex-app-id> <kubeconfig-path>"
+    exit 1
+fi
+
+# Print KUBECONFIG and DEX_APP_ID information
+echo "DEX_APP_ID: $DEX_APP_ID"
+echo "KUBECONFIG: ${KUBECONFIG:-<default>}"
+echo "-------------------------------------"
 # Other variables
+set -x
 NAMESPACE=$DEX_APP_ID
 DEX_APP_SUFFIX=${DEX_APP_ID#dex-app-}
 SPARK_DEFAULTS_CONFIGMAP="spark-defaults-conf-config-map-dex-app-${DEX_APP_SUFFIX}"
